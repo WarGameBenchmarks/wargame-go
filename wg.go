9772e9708c3d04cf3c937b83be78f82786aa0766
Ryan Rampersad <ryan.rampersad@gmail.com>
2015-12-31 11:06:19 -0600

    Added Suit
    - Added print statements

--- wg.go
+++ wg.go
@@ -28,6 +28,46 @@ const (
 	Ace
 )
 
+func (v Value) String() string {
+	s := ""
+	switch v {
+		case Two: s = "2"
+		case Three: s = "3"
+		case Four: s = "4"
+		case Five: s = "5"
+		case Six: s = "6"
+		case Seven: s = "7"
+		case Eight: s = "8"
+		case Nine: s = "9"
+		case Ten: s = "10"
+		case Jack: s = "Jack"
+		case Queen: s = "Queen"
+		case King: s = "King"
+		case Ace: s = "Ace"
+	}
+	return s
+}
+
+type Suit int
+
+const (
+	Clubs Suit = iota
+	Hearts
+	Diamonds
+	Spades
+)
+
+func (u Suit) String() string {
+	s := ""
+	switch u {
+		case Clubs: s = "Clubs"
+		case Hearts: s = "Hearts"
+		case Diamonds: s = "Diamonds"
+		case Spades: s = "Spades"
+	}
+	return s
+}
+
 func main() {
 	vs := []Value{
 		Two, Three, Four, 
@@ -35,6 +75,10 @@ func main() {
 		Eight, Nine, Ten, 
 		Jack, Queen, King, Ace}
 	for _,v := range vs {
+		fmt.Println(v)
+	}
+	ss := []Suit{Clubs, Hearts, Diamonds, Spades}
+	for _,v := range ss {
 		fmt.Println(v)
 	}
 }
