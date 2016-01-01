c5183987211def5ded6632f025d159a6d06e4524
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-01-01 04:43:50 -0600

    Added DEV flag and Logger

--- main.go
+++ main.go
@@ -4,22 +4,18 @@ import (
 	"fmt"
 )
 
+const DEVELOPMENT = false
+
 func main() {
 
-	full_deck := Deck{}
-	full_deck.Fresh()
-	full_deck.Shuffle()
-
-	p1, p2 := full_deck.Split()
-
-	fmt.Println(len(p1.cards), "cards")
-	for _,v := range p1.cards {
-		fmt.Println(v)
+	
+	if DEVELOPMENT == true {
+		fmt.Println("WarGame Go")
+		fmt.Println("\tSee `debug.log` for details.")
+	} else {
+		fmt.Println("WarGame Go")
 	}
-	fmt.Println(len(p2.cards), "cards")
-	fmt.Println("========")
-	for _,v := range p2.cards {
-		fmt.Println(v)
-	}
+
+	Game()
 
 }
