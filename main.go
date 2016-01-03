3b388c86ee31a87552349651fa2f5c04f062e85d
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-01-03 17:36:36 -0600

    Removed legacy DEVELOPMENT statements

--- main.go
+++ main.go
@@ -5,20 +5,13 @@ import (
 	"runtime"
 )
 
-const DEVELOPMENT = false
-
 func main() {
 
 	nCPU := runtime.NumCPU()
 	runtime.GOMAXPROCS(nCPU)
 	
-	if DEVELOPMENT == true {
-		fmt.Println("WarGame Go")
-		fmt.Println("\tSee `debug.log` for details.")
-	} else {
-		fmt.Println("WarGame Go")
-	}
-
+	fmt.Println("WarGame Go")
+	
 	Benchmark()
 
 }
