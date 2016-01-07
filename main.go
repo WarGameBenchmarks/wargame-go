6704a747f0a5e47d93d09aea56da215b4c326c84
Ryan Rampersad <ryan.rampersad@gmail.com>
2016-01-06 20:32:21 -0600

    Added a way to change the duration of the benchmark

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"strconv"
 	"fmt"
+	"math"
 	"runtime"
 )
 
@@ -13,22 +14,35 @@ func main() {
 	runtime.GOMAXPROCS(nCPU)
 
 	arguments := os.Args[1:]
-	
-	threads := 1
 
-	if len(arguments) >= 1 {
+	threads := 1
+	multiplier := 1.0
+
+	if len(arguments) == 2 {
+		i1, err1 := strconv.Atoi(arguments[0])
+		i2, err2 := strconv.ParseFloat(arguments[1], 64)
+		switch {
+			case err1 != nil:
+				fmt.Println(err1)
+			case err2 != nil:
+				fmt.Println(err2)
+			default:
+				threads = i1
+				multiplier = math.Abs(i2)
+		}
+	} else if len(arguments) == 1 {
 		i, err := strconv.Atoi(arguments[0])
 		if err != nil {
 			fmt.Println(err)
 		} else {
 			threads = i
-		} 
+		}
 	}
 
 	fmt.Println("WarGame Go")
 
-	fmt.Printf("settings: threads = %d\n\n", threads)
-	
-	Benchmark(threads)
+	fmt.Printf("settings: threads = %d; multiplier = %.2f\n\n", threads, multiplier)
+
+	Benchmark(threads, multiplier)
 
 }
